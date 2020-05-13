7369c5b9db0b27d816388648b4835ca8e98bf505
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-13 15:19:06 +0530

    Added component to manage input and UI Elelement

--- app.js
+++ app.js
@@ -26,49 +26,68 @@ var budgetController  = (function()
     }
 })(); */
 
-//Controller Module , IIFE
-var budgetControllerModule  = (function()
-{
-    var x = 22 ;
-    var add = function(a)
-    {
-        return x+a;
-    }
+//Budget Controller Module , IIFE
+var budgetControllerModule = (function () {})();
 
-
-    //keeping this bracket onb the same line as of return is important else it egine will think it as
-    //other block
-    return{
-        publicTest : function(b)
-        {
-            //Power of Closure,our public method will be able to access the private variable
-            return add(b);
-        }
-    }
-})();
-
-
-var UIControllerModule = (function()
-{
-
+//UI Controller
+var UIControllerModule = (function () {
+    var domStrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputButton: ".add__btn",
+    };
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(domStrings.inputType).value, // We will be getting inc or exp
+                description: document.querySelector(domStrings.inputDescription).value,
+                value: document.querySelector(domStrings.inputValue).value,
+            };
+        },
+        getDomStrings: function () {
+            return domStrings;
+        },
+    };
 })(); //IIFE
 
+// Global Controller
+var controller = (function (budgetCtrl, UICtrl) {
+    var setUpEventListeners = function () {
+        var domStrings = UICtrl.getDomStrings();
+        // Events
+        document
+            .querySelector(domStrings.inputButton)
+            .addEventListener("click", ctrlAddItem);
 
+        document.addEventListener("keypress", function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                // console.log("Enter was pressed");
+                ctrlAddItem();
+            }
+        });
+    };
 
-var controller = (function(budgetCtrl,UICtrl)
-{
-    var z = budgetCtrl.publicTest(6);
+    var ctrlAddItem = function () {
+        /* 
+           2. Add the item to the budget controller
+           3. Add the item to the UI 
+           4. Calculate the budget
+           5. DIsplay the budget on the UI
+         */
+
+        //1. Get the input data
+        var inputData = UICtrl.getInput();
+        console.log(inputData);
+    };
 
     return {
-        publicMethod : function()
-            {
-                console.log(z);
-            }
-    }
-})(budgetControllerModule,UIControllerModule); //IIFE
+        init: function () {
+            console.log("Application has started");
+            setUpEventListeners();
+        },
+    };
+})(budgetControllerModule, UIControllerModule); //IIFE
 
-/*
-controller.publicMethod()
-28
-undefined
-*/
+//App Start
+controller.init();
